2162401a7a609892df50bcf9807d5902edb760cd
Hector Medina <hector.f.medina.a@gmail.com>
2021-02-16 22:30:42 -0500

    Refactored install to use declarative configuration: -> https://setuptools.readthedocs.io/en/latest/userguide/declarative_config.html -> https://packaging.python.org/tutorials/packaging-projects/#configuring-metadata
    .toml file contains some of the package's intrinsics, like name. Static configuration goes in the .cfg file, while "dynamic" configuration goes in the .py file. Since the .cfg file does not support tags, which are used in the entry point structure to specify types for Pygments (i.e. lexer, styles), those were left in the .py file.

--- setup.py
+++ setup.py
@@ -1,24 +1,9 @@
 #! /usr/bin/env python3
 
 from setuptools import setup
-"""
-    Plugin for Pygments providing a lexer and various stylers for pretty printing code written in the Kappa agent- and rule-based modeling language.
-"""
 
 
 setup(
-    name='Pygments_Kappa_plugin',
-    version='0.1',
-    packages=['core'],
-    package_dir={'': '.'},
-    url='https://github.com/hmedina/Pygments_Kappa_plugin',
-    license='MIT',
-    author='Hector Medina',
-    author_email='hector.f.medina.a@gmail.com',
-    description='Plugin for Pygments to highlight Kappa.',
-    long_description=__doc__,
-    python_requires='>=3.6'
-    install_requires=['Pygments>=2.5.1'],
     entry_points="""
         [pygments.lexers]
         kappa_lexer = core.KappaLexer:KappaLexer
